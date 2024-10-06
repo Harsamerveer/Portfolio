69b27b15ac30e4d4c5fb332d3ba63613d2154fe9
Harsamerveer <harsamerveersingh@gmail.com>
2024-10-06 16:14:04 -0700

    Added .eslintrc.js on Sun,06,Oct,2023,4:13pm

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "env": {
+      "browser": true,
+      "es2021": true,
+      "node": true
+    },
+    "extends": [
+      "eslint:recommended",
+      "plugin:react/recommended"
+    ],
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      },
+      "ecmaVersion": 12,
+      "sourceType": "module"
+    },
+    "plugins": [
+      "react"
+    ],
+    "rules": {
+      "no-unused-vars": ["warn"],  // Change unused variables to warnings
+      "react/react-in-jsx-scope": "off"  // Example: Turn off this rule if using Next.js or modern React
+      // Add more rules as needed
+    },
+    "settings": {
+      "react": {
+        "version": "detect"  // Automatically detect the React version
+      }
+    }
+  };
+  
