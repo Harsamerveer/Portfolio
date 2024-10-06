529cc48efe0bd659195f73abf3561b6bee57421f
Harsamerveer <harsamerveersingh@gmail.com>
2024-10-06 16:29:54 -0700

    Added "react/prop-types": "off" Oct,06,2024,4:29pm

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,17 +21,14 @@ module.exports = {
     "rules": {
       "no-unused-vars": ["warn"],  // Change unused variables to warnings
       "react/react-in-jsx-scope": "off",  // Example: Turn off this rule if using Next.js or modern React
-      "react/no-unescaped-entities": "off" 
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
       // Add more rules as needed
     },
     "settings": {
       "react": {
         "version": "detect"  // Automatically detect the React version
       }
-    },
-    "letterclass":
-    {
-        "PropTypes": "string"
     }
   };
   
