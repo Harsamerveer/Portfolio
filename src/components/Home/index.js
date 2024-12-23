b94ffaf9b539e91757d22a71b674fdecc0ee08f5
Harsamerveer <harsamerveersingh@gmail.com>
2024-12-23 12:15:22 -0800

    Added Founder at RiskEvall

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,9 +15,8 @@ const Home = () => {
       link.download = 'CV.pdf';
       link.click();
     };
-
-    const[ isHovering, setIsHovering] = useState(false);
     const [letterClass, setLetterClass] = useState('text-animate')
+    const [isClicked, setIsClicked] = useState(false);  
     const openingstatement = ['H', 'i', ' ','I','\'','m',' ']
     const nameArray = ['H', 'a', 'r', 's', 'a', 'm', 'e', 'r', 'v', 'e', 'e', 'r']
     const jobArray = [   
@@ -71,13 +70,16 @@ const Home = () => {
   
     return (
       <>
+     
        <div className="container">
+            
             <div className="cv-harsamerveersingh">
               <a className="stylerefernce" href={pdf} download="CV" target="_blank" rel="noreferrer" onClick={handleDownload}>
               &lt;Sorry, PDF code is broken; fixing in progress&gt;
               <br></br>  
               <button className="cv-download-harsamerveersingh">Download CV </button>
               </a>
+             
             </div>
             <div className="page-pp">
               <img className="sub-pp-int" src={Logo} alt="Harsamerveer"/>
@@ -88,7 +90,7 @@ const Home = () => {
         <div className="home-page">
        
           <div className="text-zone">
-            
+          
             <h1><p>
               <AnimatedLetters
                 letterClass={letterClass}
@@ -108,21 +110,31 @@ const Home = () => {
               />
               <br /><br />
 
-              "Welcome to my online space! I'm thrilled to have you here. Take a moment to explore my creative journey on the left, where you'll find a showcase of my projects. Let's connect! "</p>
+              Welcome to my online space! I'm thrilled to have you here. Take a moment to explore my creative journey on the left, where you'll find a showcase of my projects. Let's connect! </p>
             
-
+          
             </h1>
             <div className="page">
-              <button className="cv-view-harsamerveersingh" onMouseEnter={() => setIsHovering(true)} 
-                      onMouseLeave={() => setIsHovering(false)}> View CV</button> 
+            <button
+              className="cv-view-harsamerveersingh"
+              onClick={() => setIsClicked(!isClicked)} // Toggle CV visibility on click
+            >
+                View CV
+              </button>
 
-              {
-              isHovering && (
-                  <div >
-                    <img className="sub-cv-int" src={LogoSubtitle} alt="Short CV" />
-                  </div>
-              )
-              }
+              {(isClicked) && (
+                <img className="sub-cv-int" src={LogoSubtitle} alt="Short CV" />
+              )}
+            <h1>Founder of RiskEval</h1>
+            <p>As the founder of <a href="https://riskevall.netlify.app" target="_blank" rel="noopener noreferrer">RiskEval</a>, a cybersecurity firm, we provide services such as:</p>
+            <div className="RiskEvallServices">
+              <ul>
+              <li><strong>Data Analysis:</strong><p>Turning complex data into actionable insights.</p></li>
+              <li><strong>Risk Mitigation:</strong><p>Developing strategies to minimize potential risks.</p></li>
+              <li><strong>Business Optimization:</strong><p>Streamlining workflows to enhance performance and outcomes.</p></li>
+              </ul>
+            </div>
+            
             </div>
           </div>
         </div>
@@ -133,3 +145,4 @@ const Home = () => {
   }
   
   export default Home  
+
